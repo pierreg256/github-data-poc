36ad967fd36ad7c00e39279b1c3fdc71d4734826
Pierre Gilot (MS) <pigilot@microsoft.com>
2021-02-18 11:35:50 +0100

    uppercase correct variable

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "__BACKENRESOURCEGROUP__"
+    resource_group_name  = "__BACKENDRESOURCEGROUP__"
     storage_account_name = "__backendstorageaccount__"
     container_name       = "__application__"
     key                  = "__application__.terraform.tfstate"
